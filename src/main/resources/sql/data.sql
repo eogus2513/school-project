7f629302b721fef7e7d3a3688dd509d2322e6cad
정대현 <eogus_2513@dsm.hs.kr>
2023-06-19 16:58:37 +0900

    예제 수정

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,5 +1,9 @@
-INSERT INTO tbl_link (link, title, user_id, created_at, expired_at)
-VALUES ('https://www.naver.com', '네이버', '31393036-3038-3364-2d30-6535352d3436', time(now()), time(now()));
+INSERT INTO tbl_link (id, link, title, user_id, created_at, expired_at)
+VALUES (1, 'https://www.google.com', '구글', '31393036-3038-3364-2d30-6535352d3436', time (now()),
+        DATE_ADD(NOW(), INTERVAL 5 DAY)) ON DUPLICATE KEY
+UPDATE expired_at = DATE_ADD(NOW(), INTERVAL 5 DAY);
 
-INSERT INTO tbl_link (link, title, user_id, created_at, expired_at)
-VALUES ('https://www.google.com', '구글', '31393036-3038-3364-2d30-6535352d3436', time(now()), time(now()));
+INSERT INTO tbl_link (id, link, title, user_id, created_at, expired_at)
+VALUES (1, 'https://www.naver.com', '네이버', '31393036-3038-3364-2d30-6535352d3436', time (now()),
+        DATE_ADD(NOW(), INTERVAL 5 DAY)) ON DUPLICATE KEY
+UPDATE expired_at = DATE_ADD(NOW(), INTERVAL 5 DAY);
