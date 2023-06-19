765dcd22e0f021e71fd9c59c9475bc153526cda8
정대현 <eogus_2513@dsm.hs.kr>
2023-06-19 20:27:40 +0900

    sql init

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -4,6 +4,6 @@ VALUES (1, 'https://www.google.com', '구글', '31393036-3038-3364-2d30-6535352d
 UPDATE expired_at = DATE_ADD(NOW(), INTERVAL 5 DAY);
 
 INSERT INTO tbl_link (id, link, title, user_id, created_at, expired_at)
-VALUES (1, 'https://www.naver.com', '네이버', '31393036-3038-3364-2d30-6535352d3436', time (now()),
+VALUES (2, 'https://www.naver.com', '네이버', '31393036-3038-3364-2d30-6535352d3436', time (now()),
         DATE_ADD(NOW(), INTERVAL 5 DAY)) ON DUPLICATE KEY
 UPDATE expired_at = DATE_ADD(NOW(), INTERVAL 5 DAY);
